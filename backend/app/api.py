9ebf1ed60af58e20fbbd17e2d4299c781035dac4
spencer.trinh <spencer.trinh@kinnate.com>
2022-10-17 15:06:34 -0700

    add upload & download endpoints for backend

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Response, Request, HTTPException, Depends, status
+from fastapi import File, UploadFile, FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
-from pathlib import Path
+from typing import List
+from starlette.responses import FileResponse
 import pandas as pd
-from datetime import datetime
+
 
 app = FastAPI()
 
@@ -25,38 +26,36 @@ def read_root():
     return {"Hello": "World"}
 
 
-parent_path = Path(f'{Path.home()}/{args.path[0]}')
-files = list(parent_path.glob(f'*{args.ft[0]}*'))
-today = datetime.now() # current date and time
-today = today.strftime("%Y_%b_%d").upper()
-ftnum = 'FT'
-lst = []
+@app.post("/upload")
+def upload(cro: str = Form(), project: str = Form(), files: List[UploadFile] = File(...)):
+    lst = []
 
-tmp_num = str(args.ft[0])
-
-for i in range(8-len(tmp_num)-2):
-    ftnum += '0'
-
-ftnum += tmp_num
-print(ftnum)
-
-
-for f in files:
-    doc, num, suffix = f.name.split('-')
-    batch = f"{num}-{suffix.split('.')[0]}"
-    lst.append([ftnum, batch, f.name, doc, args.cro[0], args.proj[0]])
-
-df = pd.DataFrame(lst, columns=[
+    for file in files:
+        try:
+            doc, num, suffix = file.filename.split('-')
+            batch = f"{num}-{suffix.split('.')[0]}"
+            lst.append([num, batch, file.filename, doc, cro, project])
+            df = pd.DataFrame(lst, columns=[
                                 'Compound_ID',
                                 'Batch_ID',
                                 'Filename',
                                 'Doc_type',
                                 'CRO',
                                 'Project'
-                                ]
-                  )
+                                ])
 
-# print(df)
+            print(df)
+            df.to_excel("file_mapping.xlsx", index=False)
+        except Exception:
+            return {"status": "There was an error uploading the file(s)"}
+    contents = [file.filename for file in files]
+    return {"status":"Successfully uploaded",
+            "contents": contents}
 
-df.to_excel(f"{parent_path}/file_mapping_output_{today}.xlsx",
-            index = False)
+
+@app.get("/download")
+def download():
+    file_name = "file_mapping.xlsx"
+    return FileResponse(file_name,
+                        media_type='application/octet-stream',
+                        filename=file_name)
