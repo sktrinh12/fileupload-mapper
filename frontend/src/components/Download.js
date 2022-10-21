aedf25c39cf28768ae1b2b6b354b1a64fd15d81b
spencer.trinh <spencer.trinh@kinnate.com>
2022-10-21 15:44:53 -0700

    use of window var

--- frontend/src/components/Download.js
+++ frontend/src/components/Download.js
@@ -15,14 +15,19 @@ function DownloadFile(props) {
     "DEC",
   ];
   const DownloadExcelFile = () => {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/download`).then((response) => {
+    fetch(`${window.REACT_APP_BACKEND_URL}/download`).then((response) => {
+      // fetch(`${process.env.REACT_APP_BACKEND_URL}/download`).then((response) => {
       response.blob().then((blob) => {
         let url = window.URL.createObjectURL(blob);
         let a = document.createElement("a");
         let today = `${
           months[date.getMonth()]
         }-${date.getDate()}-${date.getFullYear()}`;
-				let filenames = props.filenames.map(f => {return f.split('.')[0]}).join('__');
+        let filenames = props.filenames
+          .map((f) => {
+            return f.split(".")[0];
+          })
+          .join("__");
         a.href = url;
         a.download = `file-mapping-${filenames}--${today}.xlsx`;
         a.click();
