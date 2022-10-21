3392e92b4317caae9dc6387f67d570b941bd286a
spencer.trinh <spencer.trinh@kinnate.com>
2022-10-21 15:38:54 -0700

    added one main route and exclude all other paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
 import Dropzone from "./components/Dropzone";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      {/* <h3>{window.REACT_APP_BACKEND_URL}</h3>*/}
+      {/*<h3>{process.env.REACT_APP_BACKEND_URL}</h3>*/}
+      <BrowserRouter>
+        <Routes>
+          <Route
+            render={({ location }) =>
+              //This array includes pages on which user will
+              // not be redirected
+                location.pathname === "/"
+               ? null : (
+                <Navigate to="/" />
+              )
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+
       <Dropzone />
     </div>
   );
