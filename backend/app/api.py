878f9abadbb41e5d59ce23303fdcd1c5fb4fa899
spencer.trinh <spencer.trinh@kinnate.com>
2023-02-24 22:12:52 -0800

    use of stream iobytes, optional cro/proj, async and excel file parsing

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,11 +1,16 @@
-from fastapi import File, UploadFile, FastAPI, Form
+from fastapi import File, UploadFile, FastAPI, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
-from starlette.responses import FileResponse
-from os import getenv
-import pandas as pd
+
+# from starlette.responses import FileResponse
+from fastapi.responses import StreamingResponse
+from .functions import parse_xlsx, parse_generic, pd
+import io
+import xlsxwriter
 
 app = FastAPI()
+
+excel_file = io.BytesIO()
 
 origins = [
     "http://localhost:3000",
@@ -28,42 +33,37 @@ def read_root():
 
 
 @app.post("/upload")
-def upload(
-    cro: str = Form(), project: str = Form(), files: List[UploadFile] = File(...)
+async def upload(
+    cro: str = Form(default=""),
+    project: str = Form(default=""),
+    files: List[UploadFile] = File(...),
 ):
-    lst = []
 
-    for file in files:
-        try:
-            doc, num, suffix = file.filename.split("-")
-            batch = f"{num}-{suffix.split('.')[0]}"
-            lst.append([num, batch, file.filename, doc, cro, project])
-            df = pd.DataFrame(
-                lst,
-                columns=[
-                    "Compound_ID",
-                    "Batch_ID",
-                    "Filename",
-                    "Doc_type",
-                    "CRO",
-                    "Project",
-                ],
-            )
-
-            print(df)
-            directory = getenv("PARENT_DIR", "") + ("/" if getenv("PARENT_DIR") else "")
-            # with open('test', 'w') as f:
-            #     f.write(batch + '\n')
-            df.to_excel(f"{directory}file_mapping.xlsx", index=False)
-        except Exception as e:
-            return {"status": f"There was an error uploading the file(s) - {e}"}
+    df_list = []
     contents = [file.filename for file in files]
+    # print(files[0])
+    for file in files:
+        # print(file.filename)
+        try:
+            if file.filename.endswith(".xlsx"):
+                df = await parse_xlsx(file)
+                df_list.append(df)
+            else:
+                df = parse_generic(file, cro, project)
+            # Generate Excel file as byte stream
+            if file.filename.endswith(".xlsx"):
+                df = pd.concat(df_list, ignore_index=True)
+            with pd.ExcelWriter(excel_file, engine="xlsxwriter") as writer:
+                df.to_excel(writer, index=False)
+        except Exception as e:
+            raise HTTPException(status_code=404, detail=e)
     return {"status": "Successfully uploaded", "contents": contents}
 
 
 @app.get("/download")
-def download():
-    file_name = "file_mapping.xlsx"
-    return FileResponse(
-        file_name, media_type="application/octet-stream", filename=file_name
+async def download():
+    excel_file.seek(0)
+    return StreamingResponse(
+        excel_file,
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     )
