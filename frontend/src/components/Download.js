2a11ce6cccba8569a77139df8b9f0b4298c46d68
spencer.trinh <spencer.trinh@kinnate.com>
2023-02-24 22:10:35 -0800

    auto-format, use of slice on filename

--- frontend/src/components/Download.js
+++ frontend/src/components/Download.js
@@ -1,47 +1,49 @@
 function DownloadFile(props) {
-  const date = new Date();
+  const date = new Date()
+  const maxChars = 20
   const months = [
-    "JAN",
-    "FEB",
-    "MAR",
-    "APR",
-    "MAY",
-    "JUN",
-    "JUL",
-    "AUG",
-    "SEP",
-    "OCT",
-    "NOV",
-    "DEC",
-  ];
-  const DownloadExcelFile = () => {
+    'JAN',
+    'FEB',
+    'MAR',
+    'APR',
+    'MAY',
+    'JUN',
+    'JUL',
+    'AUG',
+    'SEP',
+    'OCT',
+    'NOV',
+    'DEC',
+  ]
+  const downloadExcelFile = () => {
     fetch(`${window.REACT_APP_BACKEND_URL}/download`).then((response) => {
       // fetch(`${process.env.REACT_APP_BACKEND_URL}/download`).then((response) => {
       response.blob().then((blob) => {
-        let url = window.URL.createObjectURL(blob);
-        let a = document.createElement("a");
+        let url = window.URL.createObjectURL(blob)
+        let a = document.createElement('a')
         let today = `${
           months[date.getMonth()]
-        }-${date.getDate()}-${date.getFullYear()}`;
+        }-${date.getDate()}-${date.getFullYear()}`
         let filenames = props.filenames
           .map((f) => {
-            return f.split(".")[0];
+            console.log(f)
+            return f.split('.')[0].slice(0, maxChars)
           })
-          .join("__");
-        a.href = url;
-        a.download = `file-mapping-${filenames}--${today}.xlsx`;
-        a.click();
-      });
+          .join('__')
+        a.href = url
+        a.download = `file-mapping-${filenames}--${today}.xlsx`
+        a.click()
+      })
       //window.location.href = response.url;
-    });
-  };
+    })
+  }
 
   return (
-    <div id="download-div">
+    <div id='download-div'>
       <h3>Download File Mapping</h3>
-      <button onClick={DownloadExcelFile}>Download</button>
+      <button onClick={downloadExcelFile}>Download</button>
     </div>
-  );
+  )
 }
 
-export default DownloadFile;
+export default DownloadFile
