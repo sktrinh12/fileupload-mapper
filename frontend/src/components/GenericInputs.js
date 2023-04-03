9b344c463b44dc224d9047cb7cefa99f9fc01d79
spencer.trinh <spencer.trinh@kinnate.com>
2023-04-03 11:31:07 -0700

    conditionally render the cro/proj inputs

--- frontend/src/components/GenericInputs.js
+++ frontend/src/components/GenericInputs.js
@@ -10,44 +10,51 @@ const GenericInput = ({ xlsxFiles }) => {
     batchIDs,
     fileNames,
   } = useContext(GenInputContext)
-  return xlsxFiles.length > 0 ? (
-    batchIDs.length > 0 && (
-      <table>
-        <tbody>
-          {batchIDs.map((b, i) => (
-            <tr key={`batch_${i}`}>
-              <td>
-                {/*<input
+  const hasPdf = fileNames.some((fileName) => fileName.endsWith('.pdf'))
+
+  return xlsxFiles.length > 0
+    ? batchIDs.length > 0 && (
+        <table>
+          <tbody>
+            {batchIDs.map((b, i) => (
+              <tr key={`batch_${i}`}>
+                <td>
+                  {/*<input
                     type='text'
                     value={b}
                     onChange={(e) => handleBatchIDChange(i, e.target.value)}
                   />
 								*/}
-                {b}
-              </td>
-              <td>{fileNames[i]}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    )
-  ) : (
-    <>
-      <label>
-        <b>Project</b>
-        <input
-          id='project-id'
-          type='text'
-          value={project}
-          onChange={handleChangeProj}
-        />
-      </label>
-      <label>
-        <b>CRO</b>
-        <input id='cro-id' type='text' value={cro} onChange={handleChangeCRO} />
-      </label>
-    </>
-  )
+                  {b}
+                </td>
+                <td>{fileNames[i]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )
+    : hasPdf && (
+        <>
+          <label>
+            <b>Project</b>
+            <input
+              id='project-id'
+              type='text'
+              value={project}
+              onChange={handleChangeProj}
+            />
+          </label>
+          <label>
+            <b>CRO</b>
+            <input
+              id='cro-id'
+              type='text'
+              value={cro}
+              onChange={handleChangeCRO}
+            />
+          </label>
+        </>
+      )
 }
 
 export default GenericInput
