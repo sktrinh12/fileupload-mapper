23f57c8cb4753bd3b86944580ab8c0c77b4aaeeb
spencer.trinh <spencer.trinh@kinnate.com>
2023-05-15 11:30:31 -0700

    changed port to 80

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,3 +1,3 @@
-window.REACT_APP_BACKEND_URL = 'http://localhost:8000'
+window.REACT_APP_BACKEND_URL = 'http://localhost:80'
 window.REACT_APP_ENVIRONMENT = 'DEV'
 window.REACT_APP_TEST = 'LightBlue'
